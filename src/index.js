630b468a9115aee2976b1cde9f624ebd15e04903
Daniel Frank <daniel.frank@lin-digital.com>
2019-02-01 22:10:06 -0800

    Alternate between X and O

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick()}>
+    <button className="square" onClick={props.onClick}>
           {props.value}
     </button>
   );
@@ -15,12 +15,16 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
     renderSquare(i) {
       return (
@@ -30,9 +34,9 @@ function Square(props) {
         />
       );
     }
-  
+
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
